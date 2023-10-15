12206c74cfb024cc0f0e2d78a5101e022053a14f
Luis Henrique <kakahimita@gmail.com>
2023-10-15 09:06:56 -0300

    implementando cores Xadrez

--- Xadrez/src/Xadrez/PartidaDeXadrez.java
+++ Xadrez/src/Xadrez/PartidaDeXadrez.java
@@ -10,31 +10,41 @@ import tabuleiro.Tabuleiro;
  * @author kakah
  */
 public class PartidaDeXadrez {
-    
+
     private Tabuleiro tabuleiro;
 
     public PartidaDeXadrez() {
         tabuleiro = new Tabuleiro(8, 8);
         initialSetup();
     }
-    
+
     public XadrezPeca[][] getPecas() {
         XadrezPeca[][] mat = new XadrezPeca[tabuleiro.getLinhas()][tabuleiro.getColunas()];
-        for (int i=0; i<tabuleiro.getLinhas(); i++) {
-            for(int j=0; j<tabuleiro.getColunas(); j++) {
+        for (int i = 0; i < tabuleiro.getLinhas(); i++) {
+            for (int j = 0; j < tabuleiro.getColunas(); j++) {
                 mat[i][j] = (XadrezPeca) tabuleiro.peca(i, j);
             }
         }
         return mat;
     }
-    
+
     private void colocaNovaPeca(char coluna, int linha, XadrezPeca peca) {
         tabuleiro.colocarPeca(peca, new XadrezPosicao(coluna, linha).toPosicao());
     }
-    
+
     private void initialSetup() {
-        colocaNovaPeca('b', 6, new Torre(tabuleiro, Cor.BRANCO));
-        colocaNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
-        colocaNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+        colocaNovaPeca('c', 1, new Torre(tabuleiro, Cor.BRANCO));
+        colocaNovaPeca('c', 2, new Torre(tabuleiro, Cor.BRANCO));
+        colocaNovaPeca('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+        colocaNovaPeca('e', 2, new Torre(tabuleiro, Cor.BRANCO));
+        colocaNovaPeca('e', 1, new Torre(tabuleiro, Cor.BRANCO));
+        colocaNovaPeca('d', 1, new Torre(tabuleiro, Cor.BRANCO));
+
+        colocaNovaPeca('c', 7, new Torre(tabuleiro, Cor.PRETO));
+        colocaNovaPeca('c', 8, new Torre(tabuleiro, Cor.PRETO));
+        colocaNovaPeca('d', 7, new Torre(tabuleiro, Cor.PRETO));
+        colocaNovaPeca('e', 7, new Torre(tabuleiro, Cor.PRETO));
+        colocaNovaPeca('e', 8, new Torre(tabuleiro, Cor.PRETO));
+        colocaNovaPeca('d', 8, new Rei(tabuleiro, Cor.PRETO));
     }
 }
--- Xadrez/src/aplicacao/UI.java
+++ Xadrez/src/aplicacao/UI.java
@@ -1,5 +1,6 @@
 package aplicacao;
 
+import Xadrez.Cor;
 import Xadrez.XadrezPeca;
 
 /**
@@ -7,25 +8,48 @@ import Xadrez.XadrezPeca;
  * @author kakah
  */
 public class UI {
-    
+
+    // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
     public static void printTabuleiro(XadrezPeca[][] pecas) {
-        for (int i=0; i<pecas.length; i++) {
+        for (int i = 0; i < pecas.length; i++) {
             System.out.print((8 - i) + " ");
-            for (int j=0; j<pecas.length; j++) {
+            for (int j = 0; j < pecas.length; j++) {
                 printPeca(pecas[i][j]);
             }
             System.out.println("");
         }
         System.out.println("  a b c d e f g h"); // 15:04
     }
-    
-    private static void printPeca(XadrezPeca peca) {
-        if(peca == null) {
-            System.out.print("-");
+
+    private static void printPeca(XadrezPeca piece) {
+        if (piece == null) {
+           System.out.print("-");
         } else {
-            System.out.print(peca);
+            if (piece.getCor() == Cor.BRANCO) {
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            } else {
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
         }
         System.out.print(" ");
-    }
-
+    } 
 }
